97d9ea3928d4c2648c0b9f8bc6064e7548756d30
RAMBABU <chinnak7416@gmail.com>
2024-08-09 22:56:13 +0530

    Dockerfile setup for backend

---
+++ run.sh
@@ -0,0 +1,15 @@
+if [ -z "$DB_HOST" ]; then
+  echo Environment Variable DB_HOST missing
+  exit 1
+fi
+
+if [ -z "$NEW_RELIC_APP_NAME" ]; then
+  echo Environment Variable NEW_RELIC_APP_NAME missing
+  exit 1
+fi
+
+if [ -z "$NEW_RELIC_LICENSE_KEY" ]; then
+  echo Environment Variable NEW_RELIC_LICENSE_KEY missing
+  exit 1
+
+node -r newrelic /app/index.js
